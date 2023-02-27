7f8d472fe262ac07bb36ad66eb6934d29b80b46e
Mihir0327 <108055279+Mihir0327@users.noreply.github.com>
2023-02-27 13:58:11 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,8 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
           </Head>
           <MainPage />
         </>
